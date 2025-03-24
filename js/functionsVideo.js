929a3b42ae6cd5f34cf6abfbffc328cdb67e4a0c
abenavides01 <andrepaobena@gmail.com>
2025-03-24 12:00:26 -0600

    Reproducción y vista de videos lista

--- js/functionsVideo.js
+++ js/functionsVideo.js
@@ -14,11 +14,11 @@ async function loadVideos() {
         container.innerHTML = ''; // Limpiar contenedor
 
         videos.forEach(video => {
-            const videoElement = document.createElement('a');
-            videoElement.href = `#`;
+            const videoElement = document.createElement('div');
             videoElement.classList.add('list-group-item', 'list-group-item-action');
             videoElement.innerHTML = `
                 <h5 class="mb-1">${video.title}</h5>
+                <iframe width="560" height="315" src="${video.url}" frameborder="0" allowfullscreen></iframe>
                 <p class="mb-1">${video.description}</p>
                 <button class="btn btn-warning btn-sm" onclick="editVideo('${video._id}')">Editar</button>
                 <button class="btn btn-danger btn-sm" onclick="deleteVideo('${video._id}')">Eliminar</button>
